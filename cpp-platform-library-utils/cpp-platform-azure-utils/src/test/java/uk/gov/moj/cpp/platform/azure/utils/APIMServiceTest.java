385f8f69b6812f9d977aa3463cf1553e8b89b9d2
santhosh-hmcts <santhosh.kotha@hmcts.net>
2025-09-15 14:22:09 +0100

    Make secrets scanner fail

--- cpp-platform-library-utils/cpp-platform-azure-utils/src/test/java/uk/gov/moj/cpp/platform/azure/utils/APIMServiceTest.java
+++ cpp-platform-library-utils/cpp-platform-azure-utils/src/test/java/uk/gov/moj/cpp/platform/azure/utils/APIMServiceTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 
 import javax.ws.rs.core.Response;
 
+import liquibase.pro.packaged.A;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -31,7 +32,7 @@ public class APIMServiceTest {
 
         when(restEasyClientService.post(anyString(),anyString(),anyString())).thenReturn(response);
 
-        apimService.triggerAPIMServiceWithRetryOnFailures("requestPayload", new int[]{1, 5, 10}, 3,60, "http://localhost:8080/sdrsApi","dummy-subscription-key");
+        apimService.triggerAPIMServiceWithRetryOnFailures("requestPayload", new int[]{1, 5, 10}, 3,60, "http://localhost:8080/sdrsApi","1674a16507104b749a76b19b6c837351");
 
     }
 
